7d350a10af3027dd44a2c8fe51f4330380f4450d
manoflogan <manoflogan@gmail.com>
2020-03-22 22:02:49 -0700

    Remove the unused function.

--- importer_datastore.py
+++ importer_datastore.py
@@ -22,25 +22,8 @@ logger = logging.getLogger('root')
 
 # Global variables
 OUTPUT_DIRECTORY_ROOT: str = 'output'  # Root directory in this project.
+FILE_NAME = 'datastore.csv'
 FILE_HEADER: str = None  # File Header
-
-
-def create_file_path_from_stb_title_string(stb: str, title: str,
-                                           date_str: str) -> str:
-    """
-    Constructs a file path from `std`, 'title', and 'date' as string
-
-    Args:
-        stb: stb as string
-        title: title as string
-        date_str: date as string
-
-    Returns:
-        constructed file path as string
-    """
-    return '{0}_{1}_{2}.csv'.format(stb.lower(),
-                                    title.lower().replace(' ', '_'),
-                                    date_str.replace('-', '_'))
 
 
 def write_to_a_directory():
@@ -48,8 +31,9 @@ def write_to_a_directory():
 
     1. Creates a directory output directory if does not exist.
     2. Reads the header if it not already parsed; if the header has not been
-        parsed, then a directory is created
-    3. If the header is already
+        parsed, then a file is created in append mode
+    3. If the header has already been created, then we append to the existing
+        file
     """
     output_dir: str = os.path.normpath(
         os.path.join(os.path.dirname(os.path.abspath(__file__)),
@@ -59,27 +43,21 @@ def write_to_a_directory():
 
     is_header_parsed: bool = False
     for content in fileinput.input():
+        content = content.replace('\n', '')
         if not is_header_parsed:
             FILE_HEADER = content.split('|')
             is_header_parsed = True  # Ignore the first line
         else:
-
             # Step # 2: Parse the header
             data_set = content.split('|')
             stb, title, _, date, _, _ = data_set
-            file_dir = os.path.join(
-                output_dir,
-                create_file_path_from_stb_title_string(stb, title, date))
-            import pdb
-            pdb.set_trace()
+            file_dir = os.path.join(output_dir, FILE_NAME)
             # Creating the directory per stb, title, and date
             logging.info('Directory created : {}'.format(file_dir))
             # Check if file exists
             file_exists: bool = os.path.exists(file_dir)
             with open(file_dir, 'a', encoding='utf-8') as file_object:
-                file_writer = csv.writer(file_object, delimiter='|',
-                                         escapechar='\\',
-                                         quoting=csv.QUOTE_NONE)
+                file_writer = csv.writer(file_object, delimiter='|')
                 if not file_exists:
                     file_writer.writerow(FILE_HEADER)
                 file_writer.writerow(data_set)
