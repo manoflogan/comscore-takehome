74a8beb90b6d62770d68d82cd7b2d865257e5e26
manoflogan <manoflogan@gmail.com>
2020-03-22 23:36:05 -0700

    Move to global variables.

--- importer_datastore.py
+++ importer_datastore.py
@@ -21,8 +21,13 @@ logger = logging.getLogger('root')
 
 
 # Global variables
-OUTPUT_DIRECTORY_ROOT: str = 'output'  # Root directory in this project.
+OUTPUT_DIRECTORY_ROOT: str = 'output'
+OUTPUT_DIRECTORY: str = os.path.normpath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                 OUTPUT_DIRECTORY_ROOT))
+# Root directory in this project.
 FILE_NAME = 'datastore.csv'
+OUTPUT_FILE_PATH = os.path.join(OUTPUT_DIRECTORY, FILE_NAME)
 FILE_HEADER: str = None  # File Header
 
 
@@ -35,31 +40,26 @@ def write_to_a_directory():
     3. If the header has already been created, then we append to the existing
         file
     """
-    output_dir: str = os.path.normpath(
-        os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                     OUTPUT_DIRECTORY_ROOT))
-    # Step 1: Create a directory if it does not exist.
-    os.makedirs(output_dir, exist_ok=True)
 
+    # Step 1: Create a directory if it does not exist.
+    os.makedirs(OUTPUT_DIRECTORY, exist_ok=True)
     is_header_parsed: bool = False
     for content in fileinput.input():
         content = content.replace('\n', '')
         if not is_header_parsed:
             FILE_HEADER = content.split('|')
             is_header_parsed = True  # Ignore the first line
+            with open(OUTPUT_FILE_PATH, 'a', encoding='utf-8') as file_object:
+                file_writer = csv.writer(file_object, delimiter='|')
+                file_writer.writerow(FILE_HEADER)
+            logging.info('Directory created : {}'.format(OUTPUT_FILE_PATH))
         else:
             # Step # 2: Parse the header
             data_set = content.split('|')
             stb, title, _, date, _, _ = data_set
-            file_dir = os.path.join(output_dir, FILE_NAME)
             # Creating the directory per stb, title, and date
-            logging.info('Directory created : {}'.format(file_dir))
-            # Check if file exists
-            file_exists: bool = os.path.exists(file_dir)
-            with open(file_dir, 'a', encoding='utf-8') as file_object:
+            with open(OUTPUT_FILE_PATH, 'a', encoding='utf-8') as file_object:
                 file_writer = csv.writer(file_object, delimiter='|')
-                if not file_exists:
-                    file_writer.writerow(FILE_HEADER)
                 file_writer.writerow(data_set)
 
 
